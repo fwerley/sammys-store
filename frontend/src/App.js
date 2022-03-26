ae0489d63181a02b31da8d25a75e03e4c3a5f297
Werley <werleyfn@gmail.com>
2022-03-26 12:03:50 -0300

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Sammy's Store</a>
+      </header>
+      <main>
+        Lista de produto na tela
+      </main>
+    </div>
+  );
+}
+
+export default App;
