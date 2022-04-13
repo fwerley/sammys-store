b287e9eb41b108226d6916393d2b7d74e5980b47
Werley Ferreira <werleyfn@gmail.com>
2022-04-13 13:17:44 -0300

    Adição roteamento de links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,28 @@
-import data from "./data";
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">
-          <div className="logo-desc">
-            <div>Sammy's</div>
-            <div>Store</div>
-          </div>
-        </a>
-      </header>
-      <main>
-        <h1>Produtos em destaque</h1>
-        <div className="products">
-          {data.products.map(product => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.image} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p><strong>{product.price}</strong></p>
-                <button>Adicionar ao carrinho</button>
-              </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">
+            <div className="logo-desc">
+              <div>Sammy's</div>
+              <div>Store</div>
             </div>
-          ))}
-        </div>
-      </main>
-    </div>
+          </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
